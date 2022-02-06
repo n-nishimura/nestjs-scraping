b6f36d9b06972381bcd50ea8a7c153d92fb20d5a
n-nishimura <n.nishimurara@gmail.com>
2022-02-06 09:03:45 +0900

    every ten second

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ScheduleModule } from '@nestjs/schedule';
+import { TasksModule } from './tasks/tasks.module';
 
+// https://docs.nestjs.com/techniques/task-scheduling 
+// タスクのスケジューリング
 
 @Module({
-  imports: [ScheduleModule.forRoot()
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ScheduleModule.forRoot(), TasksModule]
 })
 export class AppModule {}
